81e6019974a4a7e1a8abe1c9168d631e00e66812
Veturna <dpullo@outlook.com>
2023-03-10 12:56:31 +0100

    find max num with euclidean_algorithm

--- euclidean_algorithm.py
+++ euclidean_algorithm.py
@@ -1,7 +1,7 @@
-#sum numbers in array
-def sum(arr):
-    if arr != []:
-        return arr[0] + sum(arr[1:])
+#sum numbers in list
+def sum(list):
+    if list != []:
+        return list[0] + sum(list[1:])
     else:
         return 0
 
@@ -9,14 +9,24 @@ def sum(arr):
 print(sum([2, 4, 6, 8]))
 
 
-#count element in array
-def count(arr):
-    if arr != []:
-        return 1 + count(arr[1:])
+#count element in list
+def count(list):
+    if  list != []:
+        return 1 + count(list[1:])
     else:
         return 0
 
 #test
 print(count([2, 4, 6, 8]))
 
-#
+
+#find the biggest number
+def max_num(list):
+    if len(list) != 2:
+        sub_max = max_num(list[1:])
+        return list[0] if list[0] > sub_max else sub_max
+    else:
+        return list[0] if list[0] > list[1] else list[1]
+
+#test
+print(max_num([2, 4, 6, 8]))
